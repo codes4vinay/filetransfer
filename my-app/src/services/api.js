a8d81fa1bdec3a7b3158662375f531bfe1c2e52b
Vinay Kumar <149244101+codes4vinay@users.noreply.github.com>
2026-01-27 00:04:38 +0530

    Remove trailing slash from API_URI

--- my-app/src/services/api.js
+++ my-app/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URI = 'https://filespire-app.onrender.com/';
+const API_URI = 'https://filespire-app.onrender.com';
 
 export const uploadFile = async (data, expirationPeriod) => {
     try {
