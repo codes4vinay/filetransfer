e55e0ddfe2d762bc663f0d40f1366964327d4713
Vinay Kumar <149244101+codes4vinay@users.noreply.github.com>
2026-01-30 11:19:04 +0530

    Update image-controller.js

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -9,7 +9,7 @@ export const uploadImage = async (request, response) => {
     try {
         const file = await File.create(fileObj);
         console.log(file);
-        response.status(200).json({ path: `https://filespire-app.onrender.com/file/${file._id}` });
+        response.status(200).json({ path: `https://filespire-911562915445.asia-south2.run.app/file/${file._id}` });
     } catch (error) {
         console.error(error.message);
         response.status(500).json({ error: error.message });
